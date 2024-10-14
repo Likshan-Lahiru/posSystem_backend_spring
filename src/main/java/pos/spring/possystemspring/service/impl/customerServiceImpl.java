72f547120dbb737c1b8ed6327ac576cb0d2bbca3
lahiruileperumage <lahiru212001@gmail.com>
2024-10-14 21:13:22 +0530

    generate customer id

--- src/main/java/pos/spring/possystemspring/service/impl/customerServiceImpl.java
+++ src/main/java/pos/spring/possystemspring/service/impl/customerServiceImpl.java
@@ -1,5 +1,8 @@
 package pos.spring.possystemspring.service.impl;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,13 +13,34 @@ import pos.spring.possystemspring.service.CustomerService;
 @Service
 @Transactional
 public class customerServiceImpl implements CustomerService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public ResponseEntity<Void> saveCustomer(CustomerDto customerDto) {
             return null;
     }
 
+    @Transactional
     @Override
     public String generateCustomerID() {
-        return "C001";
+
+        TypedQuery<String> query = entityManager.createQuery(
+                "SELECT c.customerId FROM CustomerEntity c ORDER BY c.customerId DESC", String.class);
+        query.setMaxResults(1);
+
+
+        String lastCustomerId = query.getResultStream().findFirst().orElse(null);
+
+        if (lastCustomerId != null) {
+
+            int generatedCustomerId = Integer.parseInt(lastCustomerId.replace("C00-", "")) + 1;
+            return String.format("C00-%03d", generatedCustomerId);
+        } else {
+
+            return "C00-001";
+        }
     }
+
+
 }
